1b438f0ed1d9d4f125995a12397db3b15c3ea098
Thiago Costa <thiago.costa@groupon.com>
2015-10-06 15:51:16 -0300

    version 0.1

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,14 @@
-var options = [":ship: it", ":speedboat: it", ":boat: it", ":sailboat: it", ":rowboat: it", ":airplane: it", ":helicopter: it", ":steam_locomotive: it", ":tram: it", ":mountain_railway: it", ":bike: it", ":aerial_tramway: it", ":suspension_railway: it", ":mountain_cableway: it", ":tractor: it", ":blue_car: it", ":oncoming_automobile: it", ":car: it", ":red_car: it", ":taxi: it", ":oncoming_taxi: it", ":articulated_lorry: it", ":bus: it", ":oncoming_bus: it", ":police_car: it", ":oncoming_police_car: it", ":fire_engine: it", ":ambulance: it", ":minibus: it", ":truck: it", ":train: it", ":train2: it", ":bullettrain_front: it", ":bullettrain_side: it", ":light_rail: it", ":monorail: it", ":railway_car: it", ":trolleybus: it"];
-document.getElementById('new_comment_field').value = options[Math.floor(Math.random()*options.length)];
-document.querySelector("#js-new-comment-form-actions > .primary").click();
-setTimeout(function(){
-	document.location.reload(true);
-}, 2000);
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    // read `newIconPath` from request and read `tab.id` from sender
+    chrome.browserAction.setIcon({
+        path: "icon2.png",
+        tabId: sender.tab.id
+    });
+    }
+);
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+	chrome.tabs.query({active: true}, function(tabs) {
+		chrome.tabs.sendMessage(tab.id, "show_back", null);
+	});
+});
