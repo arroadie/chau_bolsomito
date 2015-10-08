b739e6f2d1642aa2f775e0f25e4b4f8675015a92
Thiago Costa <thiago.costa@groupon.com>
2015-10-08 11:08:35 -0300

    fix filter to avoid removal after bringing back

--- src/filter.js
+++ src/filter.js
@@ -7,7 +7,6 @@ function storeThis(element) {
         });
 }
 
-
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     // read `newIconPath` from request and read `tab.id` from sender
     chrome.storage.local.get(null, function(stored) {
@@ -19,11 +18,12 @@ document.getElementById("contentArea").addEventListener("DOMSubtreeModified", fu
 
 	var testElements = document.getElementsByClassName("userContentWrapper");
 
-	Array.prototype.filter.call(testElements, function(testElement){
-		if (testElement.innerText.indexOf('bolsomit') > -1 ) {
-			elem = testElement.parentElement.parentElement
-			elem.style.display = 'none';
-			storeThis(elem);
-	    }
+	Array.prototype.filter.call(testElements, function(testElement) {
+		if ((testElement.innerText.indexOf('bolsomit') > -1) && (testElement.parentElement.parentElement.className.indexOf('bolsominated') === -1)) {
+				elem = testElement.parentElement.parentElement;
+				elem.classList.add("bolsominated");
+				elem.style.display = 'none';
+				storeThis(elem);
+			}
 	});
 }, false);
